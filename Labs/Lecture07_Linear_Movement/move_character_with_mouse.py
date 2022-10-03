1f025776a35a9273b5a7cd60981c14f5635619cb
Daehyunlee@Alienware <daehyun.lee@gmail.com>
2022-10-03 09:36:36 +0900

    LEC 07 - Linear Movement added

---
+++ Labs/Lecture07_Linear_Movement/move_character_with_mouse.py
@@ -0,0 +1,47 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+
+
+def handle_events():
+    global running
+    global x, y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+    pass
+
+
+
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+# fill here
+TUK_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+arrow = load_image('hand_arrow.png')
+
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame = 0
+hide_cursor()
+
+while running:
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    arrow.draw(x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+
+    handle_events()
+
+close_canvas()
+
+
+
+
