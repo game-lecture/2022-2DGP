699233af168c0bd800a560b2d67a120d34bd3e93
Daehyunlee@Alienware <daehyun.lee@gmail.com>
2022-11-03 09:35:40 +0900

    LEC 12 게임 월드 업로드

---
+++ Labs/Lecture12_Game_World/play_state.py
@@ -0,0 +1,63 @@
+from pico2d import *
+import game_framework
+
+from grass import Grass
+from boy import Boy
+
+
+boy = None
+grass = None
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+            game_framework.quit()
+        else:
+            boy.handle_event(event)
+
+
+# 초기화
+def enter():
+    global boy, grass
+    boy = Boy()
+    grass = Grass()
+
+# 종료
+def exit():
+    global boy, grass
+    del boy
+    del grass
+
+def update():
+    boy.update()
+
+def draw_world():
+    grass.draw()
+    boy.draw()
+
+def draw():
+    clear_canvas()
+    draw_world()
+    update_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
+
+
+
+
+def test_self():
+    import play_state
+
+    pico2d.open_canvas()
+    game_framework.run(play_state)
+    pico2d.clear_canvas()
+
+if __name__ == '__main__':
+    test_self()
